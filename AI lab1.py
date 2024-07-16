1d49bf8f44c31e3556facf74185a3ca4c31e7615
ToobaPervaiz766 <toobapervaiz841@gmail.com>
2024-07-16 16:22:25 +0500

    Add files via upload

---
+++ AI lab1.py
@@ -0,0 +1,136 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello World")
+Hello World
+#hello world
+txt=input("Type something to test this out:")
+Type something to test this out:20
+print(txt)
+20
+print("Statement1")
+Statement1
+print("statement2")
+statement2
+print("statement1");print("statemnst2")
+statement1
+statemnst2
+x=1
+if x>0 :
+    print("This statements has a single space indentation")
+    print("This statements hasa single space indentations")
+
+This statements has a single space indentation
+This statements hasa single space indentations
+a=1452
+type(a)
+<class 'int'>
+b=(-4587)
+type(b)
+<class 'int'>
+c=0
+type(c)
+<class 'int'>
+g=1.03
+type(g)
+<class 'float'>
+h=-11.23
+type(g)
+<class 'float'>
+i=.34
+type(i)
+<class 'float'>
+j=2.12e-10
+type(j)
+<class 'float'>
+k=5E220
+type(k)
+<class 'float'>
+x=complex(1,2)
+type(x)
+<class 'complex'>
+print(x)
+(1+2j)
+z=1+2j
+type(z)
+<class 'complex'>
+z=1+2j
+type(z)
+<class 'complex'>
+x=True
+type(x)
+<class 'bool'>
+y=False
+type(y)
+<class 'bool'>
+strl = "string"
+print(strl)
+string
+str2="string"
+print(str2)
+string
+str3="string'
+SyntaxError: unterminated string literal (detected at line 1)
+str1='string"
+SyntaxError: unterminated string literal (detected at line 1)
+str2="days's"
+print(str2)
+days's
+str2='Days"s'
+print(str2)
+Days"s
+print("This is a backslash (\\) mark.")
+This is a backslash (\) mark.
+print("This is tab\t Key")
+This is tab	 Key
+print("These are \'single quotes\'")
+These are 'single quotes'
+print("This is a new line \nNew line")
+This is a new line 
+New line
+print(string1[0])
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    print(string1[0])
+NameError: name 'string1' is not defined
+string1="PYTHON TUTORIALS"
+>>> print(string1[0])
+P
+>>> print(string1[-15])
+Y
+>>> print(string1[14])
+L
+>>> print(string1[-1])
+S
+>>> print(string1[15])
+S
+>>> print(string1[4])
+O
+>>> print(string [16])
+Traceback (most recent call last):
+  File "<pyshell#62>", line 1, in <module>
+    print(string [16])
+NameError: name 'string' is not defined. Did you mean: 'string1'? Or did you forget to import 'string'?
+>>> my_list1=[5,12,13,14]
+>>> print(my_list1)
+[5, 12, 13, 14]
+>>> my_list2=['red','blue','black','white']
+>>> print(my_list2)
+['red', 'blue', 'black', 'white']
+>>> my_list3 =['red',12,112.12])
+SyntaxError: unmatched ')'
+>>> my_list3=['red',12,112.12]
+>>> print(my_list3)
+['red', 12, 112.12]
+>>> my_list=[]
+>>> print(my_list)
+[]
+>>> color_list=["red","blue","green","Black"]
+>>> color_list[0]
+'red'
+>>> print(color_list[0],color_list[3])
+red Black
+>>> color_list=["red","blue","green","black")
+SyntaxError: closing parenthesis ')' does not match opening parenthesis '['
+>>> color_list=["red","blue","green","black"]
+>>> print(color_list[0:2])
+['red', 'blue']
